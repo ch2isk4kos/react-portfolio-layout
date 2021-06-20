8876be194cae2184c7cd7ec8c8960cd3240c8b2d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-06-20 17:15:25 -0400

    move components directory from root into src directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Welcome Home</h1>
     </div>
   );
