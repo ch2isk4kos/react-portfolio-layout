05a07edbb73f5037cc0666f71241e7b6f1afeaf3
ch2isk4kos <ch2isk4kos@gmail.com>
2021-06-20 20:12:44 -0400

    import fontawesome hamburger icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
@@ -21,7 +23,8 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          {/* <span className="navbar-toggler-icon"></span> */}
+          <FontAwesomeIcon icon={faBars} />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
